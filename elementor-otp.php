43a6cf7e0c01d64c82cbc058bdba8f9063813de8
1do <idofri@gmail.com>
2019-03-06 19:56:04 +0200

    localization

--- elementor-otp.php
+++ elementor-otp.php
@@ -15,11 +15,16 @@ use ElementorPro\Plugin;
 class ElementorOTP {
     
     public function __construct() {
+        add_action( 'init',                                         [ $this, 'loadTextDomain' ] );
         add_action( 'elementor_pro/init',                           [ $this, 'init' ] );
         add_action( 'wp_ajax_elementor_pro_forms_send_form',        [ $this, 'ajaxSendForm' ], 1 );
         add_action( 'wp_ajax_nopriv_elementor_pro_forms_send_form', [ $this, 'ajaxSendForm' ], 1 );
     }
     
+    public function loadTextDomain() {
+        load_plugin_textdomain( 'elementor-otp', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+    }
+
     public function init() {
         $otp = new Elementor\OTP\Field();
         Plugin::instance()->modules_manager->get_modules( 'forms' )->add_component( $otp->get_name(), $otp );
