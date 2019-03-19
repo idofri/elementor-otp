d569620531aaa28a316a6d97483d0b22ad9edac8
Ido Friedlander <ido@imark.co.il>
2019-03-19 18:03:29 +0200

    extracted admin-settings to new class

---
+++ src/Admin.php
@@ -0,0 +1,52 @@
+<?php
+namespace Elementor\OTP;
+
+use Elementor\Settings;
+
+class Admin {
+
+    public function __construct() {
+        add_action( 'elementor/admin/after_create_settings/' . Settings::PAGE_ID, [ $this, 'registerAdminFields' ] );
+    }
+
+    public function registerAdminFields( Settings $settings ) {
+        $settings->add_section( Settings::TAB_INTEGRATIONS, 'twilio', [
+            'callback' => function() {
+                echo '<hr><h2>' . esc_html__( 'Twilio Verify', 'elementor-otp' ) . '</h2>';
+            },
+            'fields' => [
+                'otp_twilio_api_key' => [
+                    'label' => __( 'API Key', 'elementor-otp' ),
+                    'field_args' => [
+                        'type' => 'text',
+                        'desc' => sprintf( __( 'To integrate with our forms you need an <a href="%s" target="_blank">API Key</a>.', 'elementor-otp' ), 'https://www.twilio.com/docs/verify/api/applications' )
+                    ]
+                ]
+            ]
+        ] );
+
+        $settings->add_section( Settings::TAB_INTEGRATIONS, 'nexmo', [
+            'callback' => function() {
+                echo '<hr><h2>' . esc_html__( 'Nexmo', 'elementor-otp' ) . '</h2>';
+            },
+            'fields' => [
+                'otp_nexmo_api_key' => [
+                    'label' => __( 'API Key', 'elementor-otp' ),
+                    'field_args' => [
+                        'type' => 'text'
+                    ]
+                ],
+                'otp_nexmo_api_secret' => [
+                    'label' => __( 'API Secret', 'elementor-otp' ),
+                    'field_args' => [
+                        'type' => 'text',
+                        'desc' => sprintf( __( 'To integrate with our forms you need an <a href="%s" target="_blank">API Key</a>.', 'elementor-otp' ), 'https://developer.nexmo.com/verify/overview' )
+                    ]
+                ]
+            ]
+        ] );
+
+        do_action( 'elementor_otp/admin/settings', $settings, $this );
+    }
+
+}
